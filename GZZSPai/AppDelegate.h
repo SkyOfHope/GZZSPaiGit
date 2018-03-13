7f8abb56751f756502c9ebc6e0fc9eb254de8588
小样的天空 <1452524591@qq.com>
2018-03-13 10:55:57 +0800

    Initial Commit

---
+++ GZZSPai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZZSPai
+//
+//  Created by jiangkuiquan on 2018/3/13.
+//  Copyright © 2018年 skye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GZZSPai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZZSPai
+//
+//  Created by jiangkuiquan on 2018/3/13.
+//  Copyright © 2018年 skye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
